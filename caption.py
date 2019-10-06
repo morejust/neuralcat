6752922ab32c0731b9197b073ada3eab9aa17916
ohld <>
2019-10-07 00:51:39 +0300

    upd caption gen

--- caption.py
+++ caption.py
@@ -1,50 +1,63 @@
 import random
 
+
+ATTEMPTS = 50
+DEFAULT_CAPTION = "AI made this. Cat trained this AI. Mew the world."
+
+
 def _get_random_word(
     max_word_length=6,
     min_word_length=2,
 ):
-    seed = 'йцукенгшщзхъёэждлорпавыфячсмитьбю'
-    
+    seed = 'уеэоаыяиюйцукенгшщзхёэждлорпавыфячсмитьбю'
+
     word_length = random.randint(min_word_length, max_word_length)
     word = "".join(random.choices(seed, k=word_length))
     return word
-    
+
 
 def _get_random_cyrillic_text(
     max_words=20,
-    min_words=3
+    min_words=7
 ):
     nwords = random.randint(min_words, max_words)
-    
+
     words = [_get_random_word() for _ in range(nwords)]
-    
+
     words[random.randint(0, len(words) - 1)] = "cat"
-    
+
     return " ".join(words)
+
 
 def translate(text):
     from googletrans import Translator
     translator = Translator()
 
-    res = translator.translate(text, dest='en', src='lb')
+    res = translator.translate(text, dest='en', src='mk')
     return res.text
 
 
 def _check_valid_translation(text):
-    if "." in text:
-        return True
+    # if "." in text:
+    #     return True
     seed = 'йцукенгшщзхъёэждлорпавыфячсмитьбю'
     for s in seed:
         if s in text:
             return False
-    
+
     return True
 
+
 def generate_caption():
-    while True:
+    for _ in range(ATTEMPTS):
         text = _get_random_cyrillic_text()
+        print("TEXT:", text)
         translated = translate(text)
+        print("Translated:", translated)
         if _check_valid_translation(translated):
             return translated
-    
+    return DEFAULT_CAPTION
+
+
+if __name__ == "__main__":
+    print(generate_caption())
